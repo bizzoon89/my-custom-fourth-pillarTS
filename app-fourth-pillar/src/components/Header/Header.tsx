1c2167eb13b192c3b74192e395c0afeb805d1a09
Serhii Panchoshnyi <sergey.panchoshnyi@md.p2h.com>
2024-09-30 21:10:37 +0300

    feat(header): added part-header

--- app-fourth-pillar/src/components/Header/Header.tsx
+++ app-fourth-pillar/src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import Menu from "../Menu";
 import Logo from "../Logo";
 
@@ -12,6 +14,42 @@ export interface HeaderProps {
 }
 
 const Header = ({ logoData, menuData }: HeaderProps) => {
+
+  const [navState, setNavState] = useState<boolean>(false);
+
+  const handleNavToggle = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    setNavState(!navState);
+    if (navState) {
+      document.body.style.overflow = "auto";
+      document.body.classList.remove(`${styles.navActive}`);
+    } else {
+      document.body.classList.add(`${styles.navActive}`);
+      document.body.style.overflow = "hidden";
+    }
+  }
+
+  const isSticky = () => {
+    const header = document.querySelector('header') as HTMLElement | null;
+    
+    const scrollTop: number = window.scrollY;
+    const headerHeightWithMargin: number = 1; // Add your custom margin here if needed.
+    const sticky = headerHeightWithMargin <= scrollTop;
+
+    if (sticky) {
+      header?.classList.add(`${styles.fixedHeader}`);
+    } else {
+      header?.classList.remove(`${styles.fixedHeader}`);
+    }
+  };
+
+  // Sticky Menu Area
+  useEffect(() => {
+    window.addEventListener('scroll', isSticky);
+    return () => {
+      window.removeEventListener('scroll', isSticky);
+    };
+  }, []);
   
   return (
     <header className={styles.header}>
@@ -20,14 +58,14 @@ const Header = ({ logoData, menuData }: HeaderProps) => {
         <div className={styles.dropNav}>
           <Menu menuData={menuData}/>
         </div>
-        {/* <a
+        <a
           href="#"
           className={styles.navOpener}
           onClick={handleNavToggle}
           aria-expanded={`${navState ? true : false}`}
           aria-haspopup="true">
           <em>menu</em>
-        </a> */}
+        </a>
       </div>
     </header>
   )
