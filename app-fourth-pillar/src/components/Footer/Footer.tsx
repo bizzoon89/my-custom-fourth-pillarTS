657e4ce2ac352a7b52077da7206044473e6e3083
Serhii Panchoshnyi <sergey.panchoshnyi@md.p2h.com>
2024-10-18 18:49:44 +0300

    feat(footer link): added url footer link from api

--- app-fourth-pillar/src/components/Footer/Footer.tsx
+++ app-fourth-pillar/src/components/Footer/Footer.tsx
@@ -1,12 +1,21 @@
+import { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Link } from 'react-router-dom';
+
 import Logo from '../Logo';
 import FooterArea from '../FooterArea';
 import LinkList from '../LinkList';
 import SocialNetworks from '../SocialNetworks';
 
+import { request } from '../../services/postsService';
+
 import { ILogo } from '../Logo/Logo';
 import { IContactInfo } from '../FooterArea/FooterArea';
 import { ILink } from '../../types';
 import { ISocialLinks } from '../SocialNetworks/SocialNetworks';
+import { IServiceCards } from '../CardList/ServiceCards/ServiceCards';
+
+import { _URL_SERVICE} from '../../constants/apiUrl';
 
 import styles from './Footer.module.scss';
 
@@ -25,6 +34,20 @@ export interface FooterProps {
 }
 
 const Footer = ({ logoData, contactInfo, footerMenu, servicesMenu, addFooterMenu, socialLinkList }: FooterProps) => {
+  
+  const limit: number = 6;
+  const [posts, setPosts] = useState<IServiceCards[]>([]);
+
+  const onRequest = (url: string) => {
+    request(`${url}?_limit=${limit}`)
+      .then(response => setPosts(response))
+      .catch(error => console.error(error));
+  }
+
+  useEffect(() => {
+    onRequest(_URL_SERVICE);
+  }, [limit]);
+  
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
@@ -40,10 +63,13 @@ const Footer = ({ logoData, contactInfo, footerMenu, servicesMenu, addFooterMenu
         <div className={styles.footerNav}>
           <div className={styles.col}>
             <h6>{servicesMenu.title}</h6>
-            <LinkList
-              classList={styles.listLink}
-              linkItem={servicesMenu.menuItems}
-            />
+            <ul className={styles.listLink}>
+              {posts.map(item => (
+                <li key={uuidv4()}>
+                  <Link to={`/services/${item.id}`}>{item.title}</Link>
+                </li>
+              ))}
+            </ul>
           </div>
           <div className={styles.col}>
             <LinkList
